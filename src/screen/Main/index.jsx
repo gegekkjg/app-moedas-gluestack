0d8cc14081c099b905124df6c98e52b8a06b319f
jmmjml <116905479+jmmjml@users.noreply.github.com>
2023-11-11 05:20:03 +0000

    uuu

--- src/screen/Main/index.jsx
+++ src/screen/Main/index.jsx
@@ -1,100 +1,162 @@
-import React, { useState, useEffect } from 'react';
-import { useCallback } from 'react';
-import { StatusBar } from 'expo-status-bar';
+import React, { useState, useEffect } from "react";
+import { useCallback } from "react";
+import { StatusBar } from "expo-status-bar";
+import { Pressable, Text, TextInput, View } from "react-native";
+import { Picker } from "@react-native-picker/picker";
+import styles from "./globalStyles";
+import { AntDesign } from "@expo/vector-icons";
 import {
-  Pressable,
-  Text,
-  TextInput,
-  View,
-} from 'react-native';
-import {Picker} from '@react-native-picker/picker';
-import styles from './globalStyles';
-import { AntDesign } from '@expo/vector-icons';
+  Box,
+  Button,
+  ButtonText,
+  Center,
+  ChevronDownIcon,
+  FormControl,
+  FormControlLabel,
+  FormControlLabelText,
+  Icon,
+  Input,
+  InputField,
+  SelectBackdrop,
+  SelectContent,
+  SelectIcon,
+  SelectInput,
+  SelectItem,
+  SelectPortal,
+  SelectTrigger,
+} from "@gluestack-ui/themed";
+import { Select } from "@gluestack-ui/themed";
 
 export default function Main() {
-  const [moedaOrigem, setMoedaOrigem] = useState('BRL')
-  const [moedaDestino, setMoedaDestino] = useState('USD')
-  const [valorConvertido,setValorConvertido] = useState('')
-  const [valorOriginal, setValorOriginal] = useState('99.99999')
+  const [moedaOrigem, setMoedaOrigem] = useState("BRL");
+  const [moedaDestino, setMoedaDestino] = useState("USD");
+  const [valorConvertido, setValorConvertido] = useState("");
+  const [valorOriginal, setValorOriginal] = useState("99.99999");
 
-  const buscarHandle = async () =>{
-    let URL = `https://economia.awesomeapi.com.br/last/${moedaOrigem}-${moedaDestino}`
+  const buscarHandle = async () => {
+    let URL = `https://economia.awesomeapi.com.br/last/${moedaOrigem}-${moedaDestino}`;
     // setValorConvertido(URL)
-    try{
-      let page = await fetch(URL)
-      let json = await page.json()
-      console.log(json)
-      let indice = parseFloat(json[`${moedaOrigem}${moedaDestino}`].high)
+    try {
+      let page = await fetch(URL);
+      let json = await page.json();
+      console.log(json);
+      let indice = parseFloat(json[`${moedaOrigem}${moedaDestino}`].high);
       // setValorConvertido(indice)
-      console.log(indice)
-      let valor = parseFloat(valorOriginal)
-      let resu = ((indice*valor).toFixed(2))
-      setValorConvertido(resu)
+      console.log(indice);
+      let valor = parseFloat(valorOriginal);
+      let resu = (indice * valor).toFixed(2);
+      setValorConvertido(resu);
       // console.log(json[`USDBRL`].high)
-    } catch (error){
-      setValorConvertido(`Erro: ${error.message}`)
+    } catch (error) {
+      setValorConvertido(`Erro: ${error.message}`);
     }
-  }
-  const limparResultado = () =>{
-    setValorConvertido('')
-    setValorOriginal('33.3333')
-    setMoedaOrigem('BRL')
-    setMoedaDestino('USD')
-  }
+  };
+  const limparResultado = () => {
+    setValorConvertido("");
+    setValorOriginal("33.3333");
+    setMoedaOrigem("BRL");
+    setMoedaDestino("USD");
+  };
   return (
-    <View style={styles.container}>
-      <View style={styles.content}>
-        <Text style={styles.title}>Conversor de moedas</Text>
-        <View style={{flexDirection:'row',justifyContent:'center',alignItems:'center'}}>
-      <Text style={styles.texto}>Moeda 1</Text>
-      <View style={styles.viewInpu}>
-      <View style={styles.bordaEscolha}>
-          <Picker
-            selectedValue={moedaOrigem}
-            onValueChange={(itemValue, itemIndex) => setMoedaOrigem(itemValue)}
-            style={styles.escolha}
+    <Box w={"$full"} h={"$full"}>
+      <Center h={"$full"} m={"$9"}>
+        <FormControl>
+          <Text style={styles.title}>Conversor de moedas</Text>
+          <FormControlLabel>
+            <FormControlLabelText>Moeda de Origem</FormControlLabelText>
+          </FormControlLabel>
+
+          <Input>
+            <Select
+              w={"$full"}
+              selectedLabel={moedaOrigem}
+              onValueChange={(itemValue, itemIndex) =>
+                setMoedaOrigem(itemValue)
+              }
+            >
+              <SelectTrigger variant="outline" size="md">
+                <SelectInput placeholder="selecione uma opção" />
+                <SelectIcon>
+                  <Icon as={ChevronDownIcon} />
+                </SelectIcon>
+              </SelectTrigger>
+              <SelectPortal>
+                <SelectBackdrop />
+                <SelectContent>
+                  <SelectItem label="Real Brasileiro" value="BRL" />
+                  <SelectItem label="Dolar Americano" value="USD" />
+                  <SelectItem label="Peso Argentino" value="ARS" />
+                  <SelectItem label="Libra Estrelinha" value="GBP" />
+                  <SelectItem label="Euro" value="EUR" />
+                </SelectContent>
+              </SelectPortal>
+            </Select>
+          </Input>
+
+          <FormControlLabel mt={"$2"}>
+            <FormControlLabelText>Moeda de Destino</FormControlLabelText>
+          </FormControlLabel>
+
+          <Input>
+            <Select
+              w={"$full"}
+              selectedLabel={moedaDestino}
+              onValueChange={(itemValue, itemIndex) =>
+                setMoedaDestino(itemValue)
+              }
+            >
+              <SelectTrigger variant="outline" size="md">
+                <SelectInput placeholder="selecione uma opção" />
+                <SelectIcon>
+                  <Icon as={ChevronDownIcon} />
+                </SelectIcon>
+              </SelectTrigger>
+              <SelectPortal>
+                <SelectBackdrop />
+                <SelectContent>
+                  <SelectItem label="Real Brasileiro" value="BRL" />
+                  <SelectItem label="Dolar Americano" value="USD" />
+                  <SelectItem label="Peso Argentino" value="ARS" />
+                  <SelectItem label="Libra Estrelinha" value="GBP" />
+                  <SelectItem label="Euro" value="EUR" />
+                </SelectContent>
+              </SelectPortal>
+            </Select>
+          </Input>
+
+          <FormControlLabel mt={"$2"}>
+            <FormControlLabelText>Valor a ser convertido</FormControlLabelText>
+          </FormControlLabel>
+          <Input>
+            <InputField
+              keyboardType="numeric"
+              value={valorOriginal}
+              onChangeText={setValorOriginal}
+            />
+          </Input>
+          <Box>
+            <Button onPress={buscarHandle} mt={"$2"}>
+              <ButtonText>Converter</ButtonText>
+            </Button>
+            <Button onPress={limparResultado} mt={"$2"}>
+              <ButtonText>Limpar</ButtonText>
+            </Button>
+          </Box>
+          <Box
+            mt={"$2"}
+            flexDirection="row"
+            justifyContent="space-between"
+            h={"$12"}
+            alignItems="center"
+            p={"$3"}
+            bg="$amber300"
+            borderRadius={"$md"}
           >
-            <Picker.Item label="Real Brasileiro" value="BRL" />
-            <Picker.Item label="Dolar Americano" value="USD" />
-            <Picker.Item label="Peso Argentino" value="ARS" />
-            <Picker.Item label="Libra Estrelinha" value="GBP" />
-            <Picker.Item label="Euro" value="EUR" />
-          </Picker>
-        </View>
-      </View>
-      </View>
-      <View style={{flexDirection:'row',justifyContent:'center',alignItems:'center'}}>
-      <Text style={styles.texto}>Moeda 2</Text>
-      <View style={styles.viewInpu}>
-        <View style={styles.bordaEscolha}>
-          <Picker
-            selectedValue={moedaDestino}
-            onValueChange={(itemValue, itemIndex) => setMoedaDestino(itemValue)}
-            style={styles.escolha}
-          >
-            <Picker.Item label="Real Brasileiro" value="BRL" />
-            <Picker.Item label="Dolar Americano" value="USD" />
-            <Picker.Item label="Ouro" value="XAU" />
-            <Picker.Item label="Bitcoin" value="BTC" />
-          </Picker>
-        </View>
-      </View>
-      </View>
-      <View style={{flexDirection:'row',justifyContent:'center',alignItems:'center'}}>
-        <Text style={styles.texto}>Valor a converter:  </Text>
-      <View style={styles.viewInput}>
-        <TextInput value={valorOriginal} onChangeText={setValorOriginal} style={styles.texto} keyboardType='numeric'/>
-      </View>
-      </View>
-      <View style={{flexDirection:'row',justifyContent:'space-around',alignItems:'center'}}>
-      <Pressable style={styles.botao} onPress={buscarHandle}><Text style={{color:"#fff"}}>Buscar valor</Text></Pressable>
-      <Pressable style={styles.botao} onPress={limparResultado}><Text style={{color:"#fff"}}>Limpar valor</Text></Pressable>
-      </View>
-      <Text style={{textAlign:'center', color:"#fff"}}>{`Resultado: ${valorConvertido}`}</Text>
-      <StatusBar style="auto" />
-      </View>
-    </View>
+            <Text>Resultado:</Text>
+            <Text>{valorConvertido}</Text>
+          </Box>
+        </FormControl>
+      </Center>
+    </Box>
   );
 }
-
-
